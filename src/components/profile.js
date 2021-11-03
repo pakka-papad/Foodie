fa3808be15a668c7794ce3b4d2883f5514d9cd42
Siddharth Singh Rana <siddharth_sr@cs.iitr.ac.in>
2021-11-03 14:36:17 +0530

    profile page completed logout bug rating stars left

--- src/components/profile.js
+++ src/components/profile.js
@@ -5,13 +5,17 @@ import Footer from './Footer'
 import db, { auth } from '../firebase'
 import {getAuth} from "firebase/auth"
 import FavouritesList from './FavouritesList'
+import ReactStars from "react-rating-stars-component";
+import { Button } from '@mui/material'
+
 class Profile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       name : "na" ,
       piclink : "none", 
-      favs : []
+      favs : [],
+      userid : ""
     };
     // Binding method
     this.componentDidMount = this.componentDidMount.bind(this);
@@ -23,7 +27,7 @@ class Profile extends React.Component {
         .then(doc => {
           console.log(doc.data())
           // console.log(this.state.name);
-          this.setState(() =>({name : doc.data().displayName, piclink : doc.data().imageURL}));
+          this.setState(() =>({name : doc.data().displayName, piclink : doc.data().photoURL, userid: user.uid}));
         })
         await db.collection("users").doc(user.uid).collection("favourites").get()
         .then((querySnapshot) => {
@@ -37,13 +41,43 @@ class Profile extends React.Component {
                 favs  : [ ...prevState.favs , newE]
               })) 
           })
+          console.log(this.state.favs)
         })
         .catch((error) => console.log(error));
 
       })
+    }
 
+    ratingChanged = (e) => {
+      
+    }
 
-    
+    clearFavourites = async() => {
+      await db.collection("users").doc(this.state.userid).collection("favourites")
+      .get()
+      .then(res => {
+        res.forEach(element => {
+          element.ref.delete();
+        });
+        this.setState({favs : []})
+      });
+    }
+
+    deleteFav = async(id) => {
+      await db.collection("users").doc(this.state.userid).collection("favourites")
+      .get()
+      .then(res => {
+        res.forEach(element => {
+          console.log(element);
+          if(element.id === id){
+            element.ref.delete();
+          }
+        });
+        var filtered = this.state.favs.filter(function(element){
+          return element.id !== id;
+        })
+        this.setState({favs : filtered})
+      });
     }
     
     render() {
@@ -53,17 +87,24 @@ class Profile extends React.Component {
         <>
         
         <Header />        
-        <div class = "flex-row gap"> 
-            <div class = "flex-column  item-1">
-               <img class = "pic" src = {this.state.piclink} alt = "img not found"></img>
-               <h1 class = "name">
+        <div className = "flex-row gap"> 
+            <div className = "flex-column  item-1">
+               <img className = "pic" src = {this.state.piclink} alt = "img not found"></img>
+               <ReactStars
+                count={5}
+                // onChange={ratingChanged}
+                size={24}
+                activeColor="#ffd700"
+              />
+               <h2 className = "name">
                   {this.state.name}
-                </h1>
+               </h2>
+               <Button color="primary" onClick={this.clearFavourites}>Clear All Favourites</Button>
             </div>
-            <div class = "cards-1">
+            <div className = "cards-1">
             {this.state.favs.map(item => (
               <>
-               <FavouritesList image={item.imageURL} title={item.title} key={item.index} className = "item-list"/>
+               <FavouritesList id={item.id} image={item.imageURL} title={item.title} key={item.index} deleteFav={() => this.deleteFav(item.id)} className = "item-list"/>
               </>
               )
             )
